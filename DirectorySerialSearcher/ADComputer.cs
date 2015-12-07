18e13fd7bda7ac334a766d31ecba83fa06b1af07
Mark Strendin <mark.strendin@lskysd.ca>
2015-12-07 08:52:42 -0600

    Initial commit - this utility replaces the netboot explorer project.
    Utility that allows you to easily search all computers in the directory by name, serial, model number, and netbootguid. Replaces the netbootexplorer.

---
+++ DirectorySerialSearcher/ADComputer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectorySerialSearcher
+{
+    class ADComputer
+    {
+        public string name { get; set; }
+        public string netbootGUID { get; set; }
+        public string DN { get; set; }
+        public string SerialNumber { get; set; }
+        public string Model { get; set; }
+        
+        public ADComputer()
+        {
+        }
+    }
+}
